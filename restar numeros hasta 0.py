2fe7fcebb303128556e43dabcb7e2eb969645223
ImJalea <62162624+ItzJalea@users.noreply.github.com>
2022-05-25 18:18:20 -0400

    Create restar numeros hasta 0.py

---
+++ restar numeros hasta 0.py
@@ -0,0 +1,7 @@
+num = 0
+numero = int(input("Digite un numero: "))
+while numero <0:
+  numero = int(input("Digite un numero positivo: "))
+for num in range(numero):
+    print(numero)
+    numero= numero-1
